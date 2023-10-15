f93ebcfc05d5343501ace417c23a9fa9df0456d6
oyebayofemi <oyebayooluwafemi@gmail.com>
2023-10-15 13:30:13 -0400

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by oyebayo oluwafemi on 2023-10-15.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
