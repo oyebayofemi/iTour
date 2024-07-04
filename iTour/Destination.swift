d74564e1c8041ed33d07341f245c33ecf8b47c7a
oyebayofemi <oyebayooluwafemi@gmail.com>
2024-07-04 19:23:27 -0400

    init

---
+++ iTour/Destination.swift
@@ -0,0 +1,8 @@
+//
+//  Destination.swift
+//  iTour
+//
+//  Created by oyebayo oluwafemi on 2023-10-15.
+//
+
+import Foundation
---
+++ iTour/EditDestinationView.swift
@@ -0,0 +1,18 @@
+//
+//  EditDestinationView.swift
+//  iTour
+//
+//  Created by oyebayo oluwafemi on 2023-10-21.
+//
+
+import SwiftUI
+
+struct EditDestinationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditDestinationView()
+}
